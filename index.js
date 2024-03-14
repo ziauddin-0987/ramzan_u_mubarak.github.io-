016efac4a2a095c3ad8dbe1558322d46fc441b9a
ziauddin-0987 <ziauddink100@gmail.com>
2024-03-14 22:38:38 +0500

    this repo my first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+function fun() {
+  const a = (document.getElementById("fun_nub").value = "");
+  console.log(a);
+}
+a = [];
+function fun00() {
+  a.sort(prompt("print your value"));
+  console.log(a);
+}
+
+function funct() {}
+
+function functi() {}
