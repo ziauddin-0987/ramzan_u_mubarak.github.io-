ff41c324d4467195f16bb3f24fab88fcb8cd8064
ziauddin-0987 <ziauddink100@gmail.com>
2024-03-14 23:46:38 +0500

    print in includes the text1 value

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-function fun() {
-  const a = (document.getElementById("fun_nub").value = "");
-  console.log(a);
-}
-a = [];
-function fun00() {
-  a.sort(prompt("print your value"));
-  console.log(a);
-}
+function zia() {
+  let text1 =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam iure molestiae dolorum, natus necessitatibus expedita exercitationem repudiandae? Earum, ullam officiis unde nihil dolorum ducimus animi nulla aliquid dicta sit ea.";
 
-function funct() {}
-
-function functi() {}
+  if (text1.includes(prompt("print your text"))) {
+    console.log("this value is validede ");
+  } else {
+    console.log(" not avlibel this text");
+  }
+}
